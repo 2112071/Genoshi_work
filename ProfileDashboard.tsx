b59f6bca679f303fdd12d23924814bff747c79e5
PRIYANSHI VARSHNEY <104627426+2112071@users.noreply.github.com>
2023-12-30 09:06:59 +0530

    Create ProfileDashboard.tsx

---
+++ ProfileDashboard.tsx
@@ -0,0 +1,19 @@
+// ProfileDashboard.tsx
+import React from 'react';
+
+const ProfileDashboard: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      {/* User Profile Details */}
+      {/* Account Details */}
+      {/* Usage Metrics */}
+      {/* Recent Activity Feed */}
+      {/* Action Buttons */}
+      {/* List of Saved Graphs */}
+      {/* Link to Favourites and Collections pages */}
+      {/* ... other components */}
+    </div>
+  );
+};
+
+export default ProfileDashboard;
