57e7fd57ab68b86e181901889e624f5356f5f5c1
PRIYANSHI VARSHNEY <104627426+2112071@users.noreply.github.com>
2023-12-30 09:05:24 +0530

    Create GraphDetails.tsx

---
+++ GraphDetails.tsx
@@ -0,0 +1,17 @@
+// GraphDetails.tsx
+import React from 'react';
+
+const GraphDetails: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      {/* Graph Information */}
+      {/* Graph Visualization */}
+      {/* List of Papers */}
+      {/* Action Buttons */}
+      {/* ... other components */}
+    </div>
+  );
+};
+
+export default GraphDetails;
+
