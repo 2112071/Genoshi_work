d4ab39e608f268ddd92126b91a980276be5ca239
PRIYANSHI VARSHNEY <104627426+2112071@users.noreply.github.com>
2023-12-30 09:02:57 +0530

    Create AccountSettings.tsx

---
+++ AccountSettings.tsx
@@ -0,0 +1,17 @@
+// AccountSettings.tsx
+import React from 'react';
+
+const AccountSettings: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      {/* User Profile Update */}
+      {/* Password Management */}
+      {/* Subscription Plan */}
+      {/* Third-Party Account Linking */}
+      {/* Usage Statistics */}
+      {/* ... other components */}
+    </div>
+  );
+};
+
+export default AccountSettings;
