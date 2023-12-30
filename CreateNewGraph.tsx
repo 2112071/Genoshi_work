b338e726e0aa1f4f7488adf1caec068798b1ebf0
PRIYANSHI VARSHNEY <104627426+2112071@users.noreply.github.com>
2023-12-30 09:04:37 +0530

    Create CreateNewGraph.tsx

---
+++ CreateNewGraph.tsx
@@ -0,0 +1,15 @@
+// CreateNewGraph.tsx
+import React from 'react';
+
+const CreateNewGraph: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      {/* Graph Form */}
+      {/* Graph Visualization Preview */}
+      {/* Action Buttons */}
+      {/* ... other components */}
+    </div>
+  );
+};
+
+export default CreateNewGraph;
